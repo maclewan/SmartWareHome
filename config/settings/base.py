2eefed0beedaa6b9d6ade0c559ba2a48b847bf4d
maclewan <maciek.lewandowicz@gmail.com>
2024-05-12 22:43:11 +0200

    feat(login redirect): fix login redirect for non admin views; feat(requirements): fix Werkzeug and pyOpenSSL versions

--- config/settings/base.py
+++ config/settings/base.py
@@ -110,3 +110,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Custom vars
 UPC_DATABASE_API_KEY = config("UPC_DATABASE_API_KEY", default="")
+
+# login url
+LOGIN_URL = "/admin/login/"
