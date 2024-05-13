363b7b1d738c87a411a1815bc68e7c469ee27cb7
maclewan <maciek.lewandowicz@gmail.com>
2024-05-13 20:32:59 +0200

    feat(front end base): very simple htmls with simple routing to dummy pages

--- ware_home/supplies/urls.py
+++ ware_home/supplies/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from .views import ScannerPocView
+from .views import (
+    AddStockView,
+    DispatchView,
+    PopStockView,
+    ScannerPocView,
+    StockListView,
+)
 
 urlpatterns = [
     path("scanner-poc/", ScannerPocView.as_view(), name="scanner-poc"),
+    path("dispatch/", DispatchView.as_view(), name="dispatch"),
+    path("add-stock/", AddStockView.as_view(), name="stock-add"),
+    path("pop-stock/", PopStockView.as_view(), name="stock-pop"),
+    path("stock-list/", StockListView.as_view(), name="stock-list"),
 ]
