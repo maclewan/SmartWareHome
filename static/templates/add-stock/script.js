1d045c8403ab0ab779fddadeafacea59511b43f8
Maciej Lewandowicz <42801480+maclewan@users.noreply.github.com>
2025-04-08 23:34:55 +0200

    Feat/ pop restock (#17)
    * feat: exp date non required
    
    * fix ci
    
    * fix ci
    
    * feat: add stock with update supply option
    
    * feat: restock stock in pop view

--- static/templates/add-stock/script.js
+++ static/templates/add-stock/script.js
@@ -6,218 +6,257 @@ let productOriginalData = {}
 let productId = undefined
 
 document.addEventListener("DOMContentLoaded", () => {
-  handlePageLoaded();
+    handlePageLoaded();
 });
 document.getElementById("button-add").onclick = handleAdd
 document.getElementById("search-input").onkeyup = filterProducts
 document.getElementById("items").onchange = handleSelectChanged
 
 function handlePageLoaded() {
-  const bar_code = getUrlParam('bar_code')
-  if (bar_code === null) return;
+    const bar_code = getUrlParam('bar_code')
+    if (bar_code === null) return;
 
-  fetchAndFillProductData(bar_code)
+    fetchAndFillProductData(bar_code)
 }
 
 function handleSelectChanged() {
-  const bar_code = document.getElementById("items").value
-  if (bar_code === "") {
-    return
-  }
-  fetchAndFillProductData(bar_code)
+    const bar_code = document.getElementById("items").value
+    if (bar_code === "") {
+        return
+    }
+    fetchAndFillProductData(bar_code)
 }
 
 function fetchAndFillProductData(bar_code) {
-  mainForm.elements["bar_code"].value = bar_code;
+    mainForm.elements["bar_code"].value = bar_code;
 
-  fetch(`/api/supplies/product/${bar_code}/`)
-    .then(response => {
-      handleProductResponse(bar_code, response)
-    })
-    .catch(error => console.error('Error fetching items:', error));
+    fetch(`/api/supplies/product/${bar_code}/`)
+        .then(response => {
+            handleProductResponse(bar_code, response)
+        })
+        .catch(error => console.error('Error fetching items:', error));
+    fetch(`/api/supplies/supply/for-product/${bar_code}/`)
+        .then(response => {
+            handleSuppliesResponse(response)
+        })
+}
 
+async function handleSuppliesResponse(response) {
+    const responseData = await response.json()
+    console.log(responseData)
+    const suppliesSelect = document.getElementById("entities")
+    while (suppliesSelect.options.length > 1) {
+        suppliesSelect.remove(1);
+    }
+    if (responseData.length > 0) {
+        suppliesSelect.disabled = null
+    } else {
+        suppliesSelect.disabled = "disabled"
+    }
+    responseData.forEach(item => {
+        const option = document.createElement('option');
+        option.value = item.id;
+        option.text = `Supply id: ${item.id} - left: (${item.amount}) ${item.expiration_date === null ? '----' : '- expires ' + item.expiration_date}`;
+        suppliesSelect.appendChild(option);
+    });
 }
 
 function resetProductData() {
-  mainForm.elements["prod_name"].value = ""
-  mainForm.elements["volume"].value = ""
-  mainForm.elements["description"].value = ""
-  mainForm.elements["bar_code"].value = ""
-  mainForm.elements["amount"].value = ""
-  mainForm.elements["exp_date"].value = ""
+    mainForm.elements["prod_name"].value = ""
+    mainForm.elements["volume"].value = ""
+    mainForm.elements["description"].value = ""
+    mainForm.elements["bar_code"].value = ""
+    mainForm.elements["amount"].value = ""
+    mainForm.elements["exp_date"].value = ""
 }
 
 function resetProductVariables() {
-  productExists = false
-  productOriginalData = {}
-  productId = undefined
+    productExists = false
+    productOriginalData = {}
+    productId = undefined
 }
 
 
 function resetProductAddForm() {
-  mainForm.reset()
-  resetProductVariables()
+    mainForm.reset()
+    resetProductVariables()
 }
 
 function softResetProductAddForm() {
-  resetProductData()
-  resetProductVariables()
+    resetProductData()
+    resetProductVariables()
 }
 
 function storeOriginalProductData(responseData) {
-  productOriginalData.prod_name = responseData.name
-  productOriginalData.volume = responseData.volume
-  productOriginalData.description = responseData.description
+    productOriginalData.prod_name = responseData.name
+    productOriginalData.volume = responseData.volume
+    productOriginalData.description = responseData.description
 }
 
 function hasOriginalProductDataChanged() {
-  return ["prod_name", "volume", "description"].map(
-    (key) =>
-      productOriginalData[key] === mainForm.elements[key].value
-  ).some((val) => val === false)
+    return ["prod_name", "volume", "description"].map(
+        (key) =>
+            productOriginalData[key] === mainForm.elements[key].value
+    ).some((val) => val === false)
 }
 
 async function handleProductResponse(bar_code, response) {
-  productExists = (response.status === 200)
-  if (!productExists) {
-    showPopupMessage("partial", "Product not in database.")
+    productExists = (response.status === 200)
+    if (!productExists) {
+        showPopupMessage("partial", "Product not in database.")
+        return true
+    }
+
+    const responseData = await response.json()
+
+    mainForm.elements["prod_name"].value = responseData.name
+    mainForm.elements["volume"].value = responseData.volume
+    mainForm.elements["description"].value = responseData.description
+    mainForm.elements["bar_code"].readOnly = true
+    productId = responseData.id
+
+    storeOriginalProductData(responseData)
     return true
-  }
-
-  const responseData = await response.json()
-
-  mainForm.elements["prod_name"].value = responseData.name
-  mainForm.elements["volume"].value = responseData.volume
-  mainForm.elements["description"].value = responseData.description
-  mainForm.elements["bar_code"].readOnly = true
-  productId = responseData.id
-
-  storeOriginalProductData(responseData)
-  console.log(`Product exists with id ${productId}`)
-
-  return true
 }
 
 function getProductPayload() {
-  return {
-    name: mainForm.elements["prod_name"].value,
-    bar_code: mainForm.elements["bar_code"].value,
-    description: mainForm.elements["prod_name"].value,
-    volume: mainForm.elements["volume"].value,
-  }
+    return {
+        name: mainForm.elements["prod_name"].value,
+        bar_code: mainForm.elements["bar_code"].value,
+        description: mainForm.elements["prod_name"].value,
+        volume: mainForm.elements["volume"].value,
+    }
 }
 
 function getRequestHeaders() {
-  return {
-    'Content-Type': 'application/json',
-    'X-CSRFToken': CSRF_TOKEN
-  }
+    return {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': CSRF_TOKEN
+    }
 }
 
 async function createProduct() {
-  let payload = getProductPayload()
-  let response = await fetch("/api/supplies/product/", {
-    method: "POST",
-    headers: getRequestHeaders(),
-    body: JSON.stringify(payload)
-  })
+    let payload = getProductPayload()
+    let response = await fetch("/api/supplies/product/", {
+        method: "POST",
+        headers: getRequestHeaders(),
+        body: JSON.stringify(payload)
+    })
 
-  if (response.status !== 201) {
-    showPopupMessage("no", "Product was NOT created properly...")
-    return undefined
-  }
-  let responseData = await response.json()
-  console.log(`Product created with id ${productId}`)
-  return responseData.id
+    if (response.status !== 201) {
+        showPopupMessage("no", "Product was NOT created properly...")
+        return undefined
+    }
+    let responseData = await response.json()
+    console.log(`Product created with id ${productId}`)
+    return responseData.id
 }
 
 async function updateProduct() {
-  let payload = getProductPayload()
-  let bar_code = mainForm.elements["bar_code"].value
-  let response = await fetch(`/api/supplies/product/${bar_code}/`, {
-    method: "PATCH",
-    headers: getRequestHeaders(),
-    body: JSON.stringify(payload)
-  })
+    let payload = getProductPayload()
+    let bar_code = mainForm.elements["bar_code"].value
+    let response = await fetch(`/api/supplies/product/${bar_code}/`, {
+        method: "PATCH",
+        headers: getRequestHeaders(),
+        body: JSON.stringify(payload)
+    })
 
-  if (response.status !== 200) {
-    showPopupMessage("no", "Product was NOT updated properly...")
-    return undefined
-  }
-  let responseData = await response.json()
-  console.log(`Product (id ${productId}) updated`)
-  return responseData.id
+    if (response.status !== 200) {
+        showPopupMessage("no", "Product was NOT updated properly...")
+        return undefined
+    }
+    let responseData = await response.json()
+    console.log(`Product (id ${productId}) updated`)
+    return responseData.id
 }
 
 function filterProducts() {
-  const searchInput = document.getElementById('search-input').value.toLowerCase();
-  const products = document.querySelectorAll('.product-item');
-  let filteredCount = 0
-  let lastValue = ""
+    const searchInput = document.getElementById('search-input').value.toLowerCase();
+    const products = document.querySelectorAll('.product-item');
+    let filteredCount = 0
+    let lastValue = ""
 
-  products.forEach(product => {
-    const productName = product.innerText;
-    if (productName.toLowerCase().includes(searchInput)) {
-      product.style.display = '';
-      filteredCount++
-      lastValue = product.value
+    products.forEach(product => {
+        const productName = product.innerText;
+        if (productName.toLowerCase().includes(searchInput)) {
+            product.style.display = '';
+            filteredCount++
+            lastValue = product.value
+        } else {
+            product.style.display = 'none';
+        }
+    });
+
+    if (filteredCount === 1) {
+        document.getElementById("items").value = lastValue
+        fetchAndFillProductData(lastValue)
     } else {
-      product.style.display = 'none';
+        document.getElementById("items").value = ""
+        softResetProductAddForm()
     }
-  });
-
-  if (filteredCount === 1) {
-    document.getElementById("items").value = lastValue
-    fetchAndFillProductData(lastValue)
-  } else {
-    document.getElementById("items").value = ""
-    softResetProductAddForm()
-  }
 }
 
 async function handleAdd() {
-  if (!mainForm.checkValidity()) {
-    showPopupMessage("partial", "Fill out all fields...")
-    return
-  }
-  if (!productExists) {
-    productId = await createProduct()
-  }
-  if (productId === undefined) {
-    return
-  }
-
-  if (productExists && hasOriginalProductDataChanged()) {
-    const updatedId = await updateProduct()
-    if (updatedId !== productId) {
-      return
+    if (!mainForm.checkValidity()) {
+        showPopupMessage("partial", "Fill out all fields...")
+        return
     }
-  }
-  let payload = {
-    amount: mainForm.elements["amount"].value,
-    expiration_date: mainForm.elements["exp_date"].value === '' ? null : mainForm.elements["exp_date"].value,
-    product: productId,
-  }
-  let response = await fetch("/api/supplies/supply/", {
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json',
-      'X-CSRFToken': CSRF_TOKEN
-    },
-    body: JSON.stringify(payload)
-  })
+    if (!productExists) {
+        productId = await createProduct()
+    }
+    if (productId === undefined) {
+        return
+    }
 
-  if (response.status !== 201) {
-    showPopupMessage("no", "Supply was not created properly...")
-    return
-  }
-  let responseData = await response.json()
+    if (productExists && hasOriginalProductDataChanged()) {
+        const updatedId = await updateProduct()
+        if (updatedId !== productId) {
+            return
+        }
+    }
 
-  if (productExists) {
-    showPopupMessage("yes", `Supply for "${mainForm.elements["prod_name"].value}" (${responseData.amount} pcs)  added.`)
-  } else {
-    showPopupMessage("yes", `Product "${mainForm.elements["prod_name"].value}" (${responseData.amount} pcs) created, supply added.`)
-  }
+    const suppliesSelect = document.getElementById("entities")
+    let selectValue = suppliesSelect.value
+    if (selectValue === '') {
+        // New supply flow
+        let payload = {
+            amount: mainForm.elements["amount"].value,
+            expiration_date: mainForm.elements["exp_date"].value === '' ? null : mainForm.elements["exp_date"].value,
+            product: productId,
+        }
+        const response = await fetch("/api/supplies/supply/", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': CSRF_TOKEN
+            },
+            body: JSON.stringify(payload)
+        })
 
-  resetProductAddForm()
+        if (response.status !== 201) {
+            showPopupMessage("no", "Supply was not created properly...")
+            return
+        }
+        let responseData = await response.json()
+
+        if (productExists) {
+            showPopupMessage("yes", `Supply for "${mainForm.elements["prod_name"].value}" (${responseData.amount} pcs)  added.`)
+        } else {
+            showPopupMessage("yes", `Product "${mainForm.elements["prod_name"].value}" (${responseData.amount} pcs) created, supply added.`)
+        }
+    } else {
+        // update supply flow
+        const payload = {
+            amount_to_pop: -document.getElementById("amount").valueAsNumber  // hacky way of reusing existing endpoint for popping
+        }
+        const response = await fetch(`/api/supplies/supply/pop-amount/${selectValue}/`, {
+            method: "POST", headers: getRequestHeaders(), body: JSON.stringify(payload)
+        })
+        if (response.status !== 200) {
+            showPopupMessage("no", "Supply was not udated properly...")
+            return
+        }
+        showPopupMessage("yes", `Supply for "${mainForm.elements["prod_name"].value}" + (${payload.amount_to_pop} pcs) updated.`)
+    }
+    resetProductAddForm()
 }
--- static/templates/pop-stock/script.js
+++ static/templates/pop-stock/script.js
@@ -2,217 +2,234 @@ import {getUrlParam, showPopupMessage} from "../../common/script.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  handlePageLoaded();
+	handlePageLoaded();
 });
 document.getElementById("search-input").onkeyup = filterProducts
 document.getElementById("items").onchange = handleSelectChanged
 document.getElementById("btn-incr").onclick = () => {
-  handleIncrDecrClicked(true)
+	handleIncrDecrClicked(true)
 }
 document.getElementById("btn-decr").onclick = () => {
-  handleIncrDecrClicked(false)
+	handleIncrDecrClicked(false)
 }
 document.getElementById("button-pop").onclick = handlePopClicked
 
-function handlePageLoaded() {
-  const bar_code = getUrlParam('bar_code')
-  if (bar_code === null) return;
+document.getElementById("pop-amount-input").onchange = handleAmountChanged
 
-  fetch(`/api/supplies/product/${bar_code}/`)
-    .then(response => {
-      handleCheckProductExistsResponse(bar_code, response)
-      refreshSearchResults().then(() => {})
-    })
-    .catch(error => console.error('Error fetching items:', error));
+
+function handleAmountChanged() {
+	const input = document.getElementById("pop-amount-input")
+	const successButton = document.getElementById("button-pop")
+	if (input.valueAsNumber > 0 ) {
+		successButton.innerHTML = "Restock"
+	}
+	else {
+		successButton.innerHTML = "Pop"
+	}
+}
+
+function handlePageLoaded() {
+	const bar_code = getUrlParam('bar_code')
+	if (bar_code === null) return;
+
+	fetch(`/api/supplies/product/${bar_code}/`)
+		.then(response => {
+			handleCheckProductExistsResponse(bar_code, response)
+			refreshSearchResults().then(() => {
+			})
+		})
+		.catch(error => console.error('Error fetching items:', error));
 }
 
 function handleCheckProductExistsResponse(bar_code, response) {
-  const productExists = (response.status === 200)
-  if (!productExists) {
-    showPopupMessage("no", "Product not in database!")
-  } else {
-    document.getElementById("items").value = bar_code
-  }
+	const productExists = (response.status === 200)
+	if (!productExists) {
+		showPopupMessage("no", "Product not in database!")
+	} else {
+		document.getElementById("items").value = bar_code
+	}
 }
 
 async function filterProducts() {
-  const searchInput = document.getElementById('search-input').value.toLowerCase();
-  const products = document.querySelectorAll('.product-item');
-  let filteredCount = 0
-  let lastValue = ""
+	const searchInput = document.getElementById('search-input').value.toLowerCase();
+	const products = document.querySelectorAll('.product-item');
+	let filteredCount = 0
+	let lastValue = ""
 
-  products.forEach(product => {
-    const productName = product.innerText;
-    if (productName.toLowerCase().includes(searchInput)) {
-      product.style.display = '';
-      filteredCount++
-      lastValue = product.value
-    } else {
-      product.style.display = 'none';
-    }
-  });
+	products.forEach(product => {
+		const productName = product.innerText;
+		if (productName.toLowerCase().includes(searchInput)) {
+			product.style.display = '';
+			filteredCount++
+			lastValue = product.value
+		} else {
+			product.style.display = 'none';
+		}
+	});
 
-  document.getElementById("items").value = (filteredCount === 1) ? lastValue : ""
-  await handleSelectChanged()
+	document.getElementById("items").value = (filteredCount === 1) ? lastValue : ""
+	await handleSelectChanged()
 }
 
 async function handleSelectChanged() {
-  const bar_code = document.getElementById("items").value
-  if (bar_code === "") {
-    const tableTbody = document.getElementById("supplies-table-tbody")
+	const bar_code = document.getElementById("items").value
+	if (bar_code === "") {
+		const tableTbody = document.getElementById("supplies-table-tbody")
 
-    clearTable(tableTbody)
-    return
-  }
-  await fetchAndProcessSupplies(bar_code)
+		clearTable(tableTbody)
+		return
+	}
+	await fetchAndProcessSupplies(bar_code)
 }
 
 
 function createSupplyRow(supply, productName, productVolume) {
-  const tr = document.createElement("tr")
-  tr.className = "supp-table-row"
-  if (supply.expiration_state === "expired") {
-    tr.classList.add("expired-row")
-  } else if (supply.expiration_state === "soon_to_expire") {
-    tr.classList.add("soon-to-expire-row")
-  }
-  tr.id = `supp-table-row-${supply.id}`
+	const tr = document.createElement("tr")
+	tr.className = "supp-table-row"
+	if (supply.expiration_state === "expired") {
+		tr.classList.add("expired-row")
+	} else if (supply.expiration_state === "soon_to_expire") {
+		tr.classList.add("soon-to-expire-row")
+	}
+	tr.id = `supp-table-row-${supply.id}`
 
-  const tdId = document.createElement("td")
-  const tdName = document.createElement("td")
-  const tdAmount = document.createElement("td")
-  const tdExpDate = document.createElement("td")
-  const tdSelect = document.createElement("td")
-  const tdSelectInput = document.createElement("input")
+	const tdId = document.createElement("td")
+	const tdName = document.createElement("td")
+	const tdAmount = document.createElement("td")
+	const tdExpDate = document.createElement("td")
+	const tdSelect = document.createElement("td")
+	const tdSelectInput = document.createElement("input")
 
-  tdId.className = "table-cell"
-  tdName.className = "table-cell"
-  tdAmount.className = "table-cell"
-  tdAmount.id = `supp-amount-${supply.id}`
-  tdExpDate.className = "table-cell"
-  tdSelect.className = "table-cell supp-select-checkbox"
+	tdId.className = "table-cell"
+	tdName.className = "table-cell"
+	tdAmount.className = "table-cell"
+	tdAmount.id = `supp-amount-${supply.id}`
+	tdExpDate.className = "table-cell"
+	tdSelect.className = "table-cell supp-select-checkbox"
 
-  tdId.innerText = supply.id
-  tdName.innerText = `${productName} [${productVolume}]`
-  tdAmount.innerText = supply.amount
-  tdExpDate.innerText = supply.expiration_date
+	tdId.innerText = supply.id
+	tdName.innerText = `${productName} [${productVolume}]`
+	tdAmount.innerText = supply.amount
+	tdExpDate.innerText = supply.expiration_date
 
-  tdSelectInput.type = "radio"
-  tdSelectInput.name = "selectSupply"
-  tdSelectInput.value = supply.id
-  tdSelectInput.onclick = handleSupplySelectChanged
+	tdSelectInput.type = "radio"
+	tdSelectInput.name = "selectSupply"
+	tdSelectInput.value = supply.id
+	tdSelectInput.onclick = handleSupplySelectChanged
 
-  tdSelect.appendChild(tdSelectInput)
-  tr.appendChild(tdId)
-  tr.appendChild(tdName)
-  tr.appendChild(tdAmount)
-  tr.appendChild(tdExpDate)
-  tr.appendChild(tdSelect)
+	tdSelect.appendChild(tdSelectInput)
+	tr.appendChild(tdId)
+	tr.appendChild(tdName)
+	tr.appendChild(tdAmount)
+	tr.appendChild(tdExpDate)
+	tr.appendChild(tdSelect)
 
-  return tr
+	return tr
 }
 
 
 async function fetchAndProcessSupplies(bar_code) {
-  const product_response = await fetch(`/api/supplies/product/${bar_code}/`)
-  if (product_response.status !== 200) {
-    showPopupMessage("no", "Cannot fetch product...")
-    return
-  }
+	const product_response = await fetch(`/api/supplies/product/${bar_code}/`)
+	if (product_response.status !== 200) {
+		showPopupMessage("no", "Cannot fetch product...")
+		return
+	}
 
-  const supplies_response = await fetch(`/api/supplies/supply/for-product/${bar_code}/`)
-  if (supplies_response.status !== 200) {
-    showPopupMessage("no", "Cannot fetch supplies...")
-    return
-  }
+	const supplies_response = await fetch(`/api/supplies/supply/for-product/${bar_code}/`)
+	if (supplies_response.status !== 200) {
+		showPopupMessage("no", "Cannot fetch supplies...")
+		return
+	}
 
-  const productResponseData = await product_response.json()
-  const suppliesResponseData = await supplies_response.json()
+	const productResponseData = await product_response.json()
+	const suppliesResponseData = await supplies_response.json()
 
-  const tableTbody = document.getElementById("supplies-table-tbody")
+	const tableTbody = document.getElementById("supplies-table-tbody")
 
-  clearTable(tableTbody)
+	clearTable(tableTbody)
 
-  const supply_id = getUrlParam('supply_id')
+	const supply_id = getUrlParam('supply_id')
 
-  suppliesResponseData.forEach((supply) => {
-    const tr = createSupplyRow(supply, productResponseData.name, productResponseData.volume)
-    tableTbody.appendChild(tr)
-  })
+	suppliesResponseData.forEach((supply) => {
+		const tr = createSupplyRow(supply, productResponseData.name, productResponseData.volume)
+		tableTbody.appendChild(tr)
+	})
 
-  if (suppliesResponseData.length === 1) {
-    const input = tableTbody.querySelector("tr").querySelector("input")
-    input.checked = true
-  }
-  else if (supply_id !== null) {
-    const row = document.getElementById(`supp-table-row-${supply_id}`)
-    if (row !== null) {
-      const input = row.querySelector("input")
-      input.checked = true
-    }
-  }
+	if (suppliesResponseData.length === 1) {
+		const input = tableTbody.querySelector("tr").querySelector("input")
+		input.checked = true
+	} else if (supply_id !== null) {
+		const row = document.getElementById(`supp-table-row-${supply_id}`)
+		if (row !== null) {
+			const input = row.querySelector("input")
+			input.checked = true
+		}
+	}
 }
 
 function handleSupplySelectChanged() {
-  document.getElementById("pop-amount-input").value = "1.0"
+	document.getElementById("pop-amount-input").value = "-1"
 }
 
 function clearTable(tableTbody) {
-  const tableRowsCount = tableTbody.getElementsByTagName('tr').length;
+	const tableRowsCount = tableTbody.getElementsByTagName('tr').length;
 
-  for (let i = tableRowsCount - 1; i >= 0; i--) {
-    tableTbody.deleteRow(i)
-  }
+	for (let i = tableRowsCount - 1; i >= 0; i--) {
+		tableTbody.deleteRow(i)
+	}
 }
 
 function handleIncrDecrClicked(increment) {
-  const selectedInput = document.querySelector('input[name="selectSupply"]:checked')
-  if (selectedInput === null) {
-    return
-  }
-  const amount = document.getElementById(`supp-amount-${selectedInput.value}`).innerText
+	const selectedInput = document.querySelector('input[name="selectSupply"]:checked')
+	if (selectedInput === null) {
+		return
+	}
+	const amount = document.getElementById(`supp-amount-${selectedInput.value}`).innerText
 
-  const input = document.getElementById("pop-amount-input")
-  const newValue = input.valueAsNumber + 0.5 * (increment ? 1 : -1)
-  if (newValue <= 0 || newValue > Number(amount)) {
-    return
-  }
-  input.value = newValue.toFixed(1)
-
+	const input = document.getElementById("pop-amount-input")
+	let newValue = input.valueAsNumber + 0.5 * (increment ? 1 : -1)
+	if (-newValue > Number(amount)) {
+		return
+	}
+	if (newValue === 0) {
+		newValue = increment ? 0.5 : -0.5
+	}
+	input.value = newValue.toFixed(1)
+	handleAmountChanged()
 }
 
 function getRequestHeaders() {
-  return {
-    'Content-Type': 'application/json', 'X-CSRFToken': CSRF_TOKEN
-  }
+	return {
+		'Content-Type': 'application/json', 'X-CSRFToken': CSRF_TOKEN
+	}
 }
 
 function refreshSearchResults() {
-  return handleSelectChanged()
+	return handleSelectChanged()
 }
 
 async function handlePopClicked() {
-  const selectedInput = document.querySelector('input[name="selectSupply"]:checked')
-  if (selectedInput === null) {
-    showPopupMessage("partial", "Supply not selected...")
-    return
-  }
-  const selectedSupplyId = selectedInput.value
-  const payload = {
-    amount_to_pop: document.getElementById("pop-amount-input").valueAsNumber
-  }
+	const selectedInput = document.querySelector('input[name="selectSupply"]:checked')
+	if (selectedInput === null) {
+		showPopupMessage("partial", "Supply not selected...")
+		return
+	}
+	const selectedSupplyId = selectedInput.value
+	const payload = {
+		amount_to_pop: -document.getElementById("pop-amount-input").valueAsNumber
+	}
 
-  const response = await fetch(`/api/supplies/supply/pop-amount/${selectedSupplyId}/`, {
-    method: "POST", headers: getRequestHeaders(), body: JSON.stringify(payload)
-  })
+	const response = await fetch(`/api/supplies/supply/pop-amount/${selectedSupplyId}/`, {
+		method: "POST", headers: getRequestHeaders(), body: JSON.stringify(payload)
+	})
 
-  if (response.status === 200) {
-    showPopupMessage("yes", "Supply popped properly.")
-  } else if (response.status === 204) {
-    showPopupMessage("yes", "Supply removed.")
-  } else {
-    showPopupMessage("no", "Error, cannot pop supply properly...")
-  }
+	if (response.status === 200) {
+		showPopupMessage("yes", "Supply popped properly.")
+	} else if (response.status === 204) {
+		showPopupMessage("yes", "Supply removed.")
+	} else {
+		showPopupMessage("no", "Error, cannot pop supply properly...")
+	}
 
-  await refreshSearchResults()
+	await refreshSearchResults()
 }
